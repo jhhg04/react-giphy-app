9222385a37b5ff256566de7398223d53c627dbcd
jhhg0 <jhherrera@ccss.sa.cr>
2021-09-01 16:52:23 -0500

    first commit

---
+++ docs/precache-manifest.ed5e9b336e0fb4e5fcbb4c54b16c6020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d862027b979e872a9b12db66be6b3062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5fb6f569b167190a497",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fb6f569b167190a497",
+    "url": "/static/js/main.eade6ad3.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
