7b6a5a526d201279e11b3b1152e55d87bd7c880a
jhhg0 <jhherrera@ccss.sa.cr>
2021-09-02 18:21:47 -0500

    Add test

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddCategory } from '../../components/AddCategory';
+
+describe('Pruebas en <AddCategory />', () => {
+  const setCategories = () => {};
+  test('Debe de mostrarse correctamente', () => {
+    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
