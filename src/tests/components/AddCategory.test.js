3df3043956d81d854a17516d3e999d8f50bfd444
jhhg0 <jhherrera@ccss.sa.cr>
2021-09-07 17:11:41 -0500

    Finish video 93

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,8 +4,18 @@ import { AddCategory } from '../../components/AddCategory';
 
 describe('Pruebas en <AddCategory />', () => {
   const setCategories = () => {};
+  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
   test('Debe de mostrarse correctamente', () => {
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('debe de cambiar la caja de texto ', () => {
+    const input = wrapper.find('input');
+    const value = 'Hola Mundo';
+
+    input.simulate('change', { target: { value: value } });
+
+    expect(wrapper.find('p').text().trim()).toBe(value);
   });
 });
